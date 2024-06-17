97e4a3f57346aaba9400675b29be4c8db867773b
Fardeen S <fardeenclan@gmail.com>
2024-06-17 23:21:06 +0530

    added auth with clekr

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,26 @@
+"use client";
 import PodCastCard from "@/components/PostCastCard";
-import { Button } from "@/components/ui/button";
 import { podcastData } from "@/constants";
 
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { SignOutButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { SignedIn } from "@clerk/clerk-react";
+
 const Home = () => {
+  // const tasks = useQuery(api.tasks.get);
   return (
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+          {/* {tasks?.map(({ _id, text }) => (
+            <div className="text-white-1" key={_id}>
+              {text}
+            </div>
+          ))} */}
+        </div>
         <div className="podcast_grid">
           {podcastData.map((podcast) => (
             <PodCastCard
---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://magnetic-oarfish-9.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+export default authConfig;
